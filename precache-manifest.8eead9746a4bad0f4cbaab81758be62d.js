1a334bc9f3f04310bb606a7be82b402650b7fef1
bstyx <sebastianludtke@protonmail.com>
2019-12-22 00:28:54 +0100

    Updates

---
+++ precache-manifest.8eead9746a4bad0f4cbaab81758be62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1827a58191dfff98b5151fd71ed05ea9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b98e3f451016e17151f7",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "b98e3f451016e17151f7",
+    "url": "/robofriends/static/js/main.dc9e2bf5.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
